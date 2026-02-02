5ecf1aa548c94519c930ad97f5b28328e3f2db5b
Mossann425 <begattya425@gmail.com>
2026-02-02 16:48:57 +0900

    1画面目、2画面目のapiファイル追加

---
+++ functions/api/users.js
@@ -0,0 +1,12 @@
+// GET /api/users でアクセスされたら動く
+export async function onRequestGet(context) {
+  const db = context.env.DB; // 操作担当者が設定した"DB"という名前を使う
+
+  try {
+    // 全ユーザーを取得
+    const { results } = await db.prepare("SELECT * FROM Users").all();
+    return Response.json(results);
+  } catch (e) {
+    return Response.json({ error: e.message }, { status: 500 });
+  }
+}
